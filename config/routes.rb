876cc828843e0c351fc52cd9c0bb470066df5638
madebytechnology <milena.szklarska@gmail.com>
2014-03-26 23:47:25 +0100

    NEWEST

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 MyGists::Application.routes.draw do
-  resources :gists
+resources :gists
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'gists#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
